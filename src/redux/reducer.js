73435950e4e86e836b00ed1c9b2870b109d0baa9
Basofi <idkyo7@gmail.com>
2021-01-19 20:40:56 +0700

    made some base layout and start configure redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,14 @@
+import { ADD_TODO, UPDATE_TODO, DELETE_TODO } from './actions'
+
+export let reducer = (state, action) {
+    switch (key) {
+        case ADD_TODO:
+            break;
+        case UPDATE_TODO:
+            break;
+        case DELETE_TODO:
+            break;
+        default:
+            break;
+    }
+}
